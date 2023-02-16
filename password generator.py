1393211a1bb20157abc3f503ec271d995138e762
Nilesh Hadalgi <57908107+nileshhadalgi016@users.noreply.github.com>
2023-02-16 22:00:50 +0530

    Initial commit

--- password generator.py
+++ password generator.py
@@ -1,9 +1,11 @@
 import random
 import string
 
+
 def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for i in range(length))
     return password
+
 
 print("Random password:", generate_password(12))
