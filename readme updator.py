bb989239c688c1fc390a9eb1ad9f37a2c7dbfd12
Nilesh Hadalgi <57908107+nileshhadalgi016@users.noreply.github.com>
2023-02-16 22:55:33 +0530

    Initial commit

--- readme updator.py
+++ readme updator.py
@@ -5,7 +5,6 @@ with open("README.md", "r") as readme_file:
     readme_content = readme_file.read()
     print(f"README content:\n{readme_content}\n")
 
-
 # Get the names of folders in the current directory and sort them alphabetically
 current_dir = os.getcwd()
 folder_names = sorted([f for f in os.listdir(current_dir)
@@ -15,18 +14,12 @@ folder_names = sorted([f for f in os.listdir(current_dir)
 new_section = "\nFolders:\n"
 for folder_name in folder_names:
     folder_path = os.path.join(".", folder_name)
-    main_py_path = os.path.join(folder_path, "main.py")
-    if os.path.isfile(main_py_path):
-        main_py_link = f"[main.py]({main_py_path})"
-        folder_link = f"[{folder_name}]({folder_path})"
-        new_section += f"- {folder_link}: {main_py_link}\n"
-    else:
-        folder_link = f"[{folder_name}]({folder_path})"
-        new_section += f"- {folder_link}\n"
+    folder_link = f"[{folder_name.replace(' ', '%20')}]({folder_path})"
+    new_section += f"- {folder_link}\n"
 new_section += "\n"
 
 # Insert the new section into the README.md file
-start_marker = "### Codes Here:"
+start_marker = "### Start Here"
 end_marker = "### End Here"
 start_index = readme_content.find(start_marker)
 if start_index != -1:
